a06b2f6d49e0b1340706b1a58a728b4f31984d84
Tea Galli <gall2590@mylaurier.ca>
2018-10-07 22:10:41 -0400

    update

--- plot1.R
+++ plot1.R
@@ -25,8 +25,7 @@ PowerConsumptionDataSet$DateTime <- strptime(PowerConsumptionDataSet$DateTime, "
 
 # Create Plot 1
 # -----------------------------------------------------------------------------------------------------------------------------------
-png("plot1.png", width = 480,height = 480)
-par(mar = c(5,4,3,2) + 0.1)       
+png("plot1.png", width = 480,height = 480)       
 hist(PowerConsumptionDataSet$Global_active_power, col = "red", main = "Global Active Power", 
 	xlab = "Global Active Power (kilowatts)", cex.lab = 0.8, cex.main = 1, cex.axis = 0.8)
 dev.off()
